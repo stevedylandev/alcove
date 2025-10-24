934f715dabb4dc98aa29e4ba3c1a701cf8c97c4c
Steve <contact@stevedylan.dev>
2025-10-24 19:56:13 -0400

    chore: added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 	return (
-		<main>
-			<h1>ALCOVE</h1>
-			<p>Coming soon</p>
+		<main className="min-h-screen w-full items-center justify-center flex-col flex bg-[#121113] text-white font-mono gap-2">
+			<h1 className="text-6xl font-bold">ALCOVE</h1>
+			<p className="text-lg">Coming soon</p>
 		</main>
 	);
 }
