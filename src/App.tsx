7afa918ba20e601a3f96b45b01ab4e3d6d753666
Steve <contact@stevedylan.dev>
2025-10-24 20:01:08 -0400

    chore: setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
 function App() {
 	return (
-		<main className="min-h-screen w-full items-center justify-center flex-col flex bg-[#121113] text-white font-mono gap-2">
-			<h1 className="text-6xl font-bold">ALCOVE</h1>
-			<p className="text-lg">Coming soon</p>
-		</main>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<main className="min-h-screen w-full items-center justify-center flex-col flex gap-2">
+				<h1 className="text-6xl font-bold">ALCOVE</h1>
+				<p className="text-lg">Coming soon</p>
+			</main>
+		</ThemeProvider>
 	);
 }
 
