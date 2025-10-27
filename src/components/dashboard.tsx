d7cdfcd352e46a81e78a187bed586be88d14cbff
Steve <contact@stevedylan.dev>
2025-10-27 16:22:05 -0400

    chore: added dashboard components

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,64 @@
+import { useEvolu } from "../main";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+function Dashboard() {
+	const { insert, update } = useEvolu();
+
+	return (
+		<main className="min-h-screen w-full items-center justify-center flex-col flex gap-2">
+			<SidebarProvider
+				style={
+					{
+						"--sidebar-width": "350px",
+					} as React.CSSProperties
+				}
+			>
+				<AppSidebar />
+				<SidebarInset>
+					<header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+								<BreadcrumbItem>
+									<BreadcrumbPage>Inbox</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</header>
+					<div className="flex flex-1 flex-col gap-4 p-4">
+						{Array.from({ length: 24 }).map((_, index) => (
+							<div
+								key={index}
+								className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+							/>
+						))}
+					</div>
+				</SidebarInset>
+			</SidebarProvider>
+		</main>
+	);
+}
+
+export default Dashboard;
